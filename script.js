c4e1d94aadad27b2b66a28dd3de008c02d63a9fe
Victor Fountains <vfuentes21144@columbushs.com>
2020-02-14 01:13:44 -0500

    school

--- script.js
+++ script.js
@@ -2,12 +2,19 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 
+
+
 const circle = {
-    x: 200,
-    y: 200,
+    x: 500,
+    y: 500,
     size: 15,
     dx: 5,
     dy: 4,
+}
+
+function startGame() {
+    drawCircle();
+    update();
 }
 
 function drawCircle() {
@@ -26,13 +33,132 @@ function update() {
     circle.y += circle.dy;
 
     if (circle.x + circle.size > canvas.width || circle.x - circle.size < 0) {
+        circle.x = 700
+        circle.y = 400
         circle.dx *= -1;
     } else if (circle.y + circle.size > canvas.height || circle.y - circle.size < 0) {
         circle.dy *= -1;
+
     }
+
+
 
     requestAnimationFrame(update);
 
 }
 
+
+
+const player1 = {
+    x: 1,
+    y: 450,
+    dx: 0,
+    dy: 0,
+    speed: 8,
+    width: 10,
+    height: 170
+}
+
+const player2 = {
+    x: 1904,
+    y: 450,
+    dx: 0,
+    dy: 0,
+    speed: 8,
+    width: 10,
+    height: 170
+}
+
+function drawPlayer1() {
+    ctx.fillStyle = "#ffffff"
+    ctx.fillRect(player1.x, player1.y, player1.width, player1.height)
+    ctx.fill;
+
+}
+
+
+
+function drawPlayer2() {
+    ctx.fillStyle = "#ffffff"
+    ctx.fillRect(player2.x, player2.y, player2.width, player2.height)
+    ctx.fill;
+    console.log(123)
+}
+
+function newPos1() {
+    player1.x += player1.dx;
+    player1.y += player1.dy;
+}
+
+function newPos2() {
+    player2.x += player1.dx;
+    player2.y += player1.dy;
+}
+
+function updatePlayer1() {
+    ctx.clearRect(0, 0, player1.width, player2.height);
+    drawPlayer1();
+    requestAnimationFrame(updatePlayer1);
+
+}
+
+function updatePlayer2() {
+    ctx.clearRect(0, 0, player2.width, player2.height);
+    drawPlayer2();
+    requestAnimationFrame(updatePlayer2);
+}
+
+
+
+
 update();
+updatePlayer1();
+updatePlayer2();
+
+function moveDownPlayer1() {
+    player1.y += player1.speed;
+}
+
+function moveUpPlayer1() {
+    player1.y -= player1.speed;
+}
+
+function moveDownPlayer2() {
+    player2.y += player2.speed;
+}
+
+function moveUpPlayer2() {
+    player2.y -= player2.speed;
+}
+
+
+document.addEventListener('keydown', keyDown)
+document.addEventListener('keyup', keyUp)
+
+function keyDown(e) {
+    console.log(e.key);
+    if (e.key === "ArrowUp")
+        moveUpPlayer1();
+    else if (e.key === "ArrowDown")
+        moveDownPlayer1();
+    else if (e.key === "w")
+        moveUpPlayer2();
+    else if (e.key === "s")
+        moveDownPlayer2();
+}
+
+function keyUp(e) {
+    if (
+        e.key == "ArrowUp" ||
+        e.key == "ArrowDown"
+    ) {
+        player1.dy = 0;
+    } else if (
+        e.key == "w" ||
+        e.key == "s"
+    ) {
+        player2.dy = 0;
+    }
+}
+
+keyDown();
