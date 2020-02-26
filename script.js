e812da660d30f252856bdc617a3ba3767f2aa895
Victor Fountains <vfuentes21144@columbushs.com>
2020-02-25 19:29:13 -0500

    L

--- script.js
+++ script.js
@@ -5,9 +5,21 @@ const ctx = canvas.getContext('2d');
 const circle = {
     x: 500,
     y: 500,
-    size: 15,
+    size: 10,
     dx: 5,
     dy: 4,
+}
+
+const player1Score = {
+    x: 550,
+    y: 200,
+    value: 0
+}
+
+const player2Score = {
+    x: 1200,
+    y: 200,
+    value: 0
 }
 
 function startGame() {
@@ -15,17 +27,33 @@ function startGame() {
     update();
 }
 
-function drawCircle() {
+function drawGame() {
     ctx.beginPath()
 
     ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2, true);
     ctx.fillstyle = "#FF0000";
     ctx.fill();
+
+    ctx.fillStyle = "#ffffff"
+    ctx.fillRect(player1.x, player1.y, player1.width, player1.height)
+    ctx.fill;
+
+    ctx.fillStyle = "#ffffff"
+    ctx.fillRect(player2.x, player2.y, player2.width, player2.height)
+    ctx.fill;
+
+    ctx.fillStyle = "#ffffff";
+    ctx.font = '30px serif';
+    ctx.fillText(player1Score.value, player1Score.x, player1Score.y);
+
+    ctx.fillStyle = "#ffffff";
+    ctx.font = '30px serif';
+    ctx.fillText(player2Score.value, player2Score.x, player2Score.y);
 }
 
 function update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawCircle();
+    drawGame();
 
     circle.x += circle.dx;
     circle.y += circle.dy;
@@ -34,7 +62,6 @@ function update() {
 
         if (circle.y > player1.y && circle.y < player1.y + player1.height) {
             circle.dx *= -1;
-            console.log('hit')
         } else if (circle.y > player2.y && circle.y < player2.y + player2.height) {
             circle.dx *= -1;
         } else {
@@ -75,20 +102,7 @@ const player2 = {
     height: 170
 }
 
-function drawPlayer1() {
-    ctx.fillStyle = "#ffffff"
-    ctx.fillRect(player1.x, player1.y, player1.width, player1.height)
-    ctx.fill;
 
-}
-
-
-
-function drawPlayer2() {
-    ctx.fillStyle = "#ffffff"
-    ctx.fillRect(player2.x, player2.y, player2.width, player2.height)
-    ctx.fill;
-}
 
 function newPos1() {
     player1.x += player1.dx;
@@ -102,85 +116,89 @@ function newPos2() {
 
 function updatePlayer1() {
     ctx.clearRect(0, 0, player1.width, player1.height);
-    drawPlayer1();
+    drawGame();
     requestAnimationFrame(updatePlayer1);
 
 }
 
 function updatePlayer2() {
     ctx.clearRect(0, 0, player2.width, player2.height);
-    drawPlayer2();
+    drawGame();
     requestAnimationFrame(updatePlayer2);
 }
 
-function drawScores(){
-   ctx.beginPath();
-   ctx.fillStyle = "#ffffff";
-    ctx.font = 'px serif';
-  ctx.fillText("Hello world", 600, 200);
+
+function updateScores() {
+    drawGame();
+    requestAnimationFrame(updateScores);
 }
-drawScores();
+updateScores();
 
 
-function scorePoint(){
-    if (Math.sign(circle.dx) === -1){
-
+function scorePoint() {
+    if (Math.sign(circle.dx) === -1) {
+        player2Score.value += 1;
         circle.dx *= -1;
-    }
-    else if (Math.sign(circle.dx) === 1){
+    } else if (Math.sign(circle.dx) === 1) {
+        player1Score.value += 1;
         circle.dx *= -1;
-    }
-}
+        //     } else if (player1Score.value >= 5 || player2Score.value >= 5) {
+        //         checkWin();
+        //     }
+        // }
 
-update();
-updatePlayer1();
-updatePlayer2();
+        // function checkWin() {
+        //     console.log('blah')
+        // }
 
-function moveDownPlayer1() {
-    player1.y += player1.speed;
-}
+        update();
+        updatePlayer1();
+        updatePlayer2();
 
-function moveUpPlayer1() {
-    player1.y -= player1.speed;
-}
+        function moveDownPlayer1() {
+            player1.y += player1.speed;
+        }
 
-function moveDownPlayer2() {
-    player2.y += player2.speed;
-}
+        function moveUpPlayer1() {
+            player1.y -= player1.speed;
+        }
 
-function moveUpPlayer2() {
-    player2.y -= player2.speed;
-}
+        function moveDownPlayer2() {
+            player2.y += player2.speed;
+        }
+
+        function moveUpPlayer2() {
+            player2.y -= player2.speed;
+        }
 
 
-document.addEventListener('keydown', keyDown)
-document.addEventListener('keyup', keyUp)
+        document.addEventListener('keydown', keyDown)
+        document.addEventListener('keyup', keyUp)
 
-function keyDown(e) {
-    console.log(e.key);
-    if (e.key === "ArrowUp")
-        moveUpPlayer1();
-    else if (e.key === "ArrowDown")
-        moveDownPlayer1();
-    else if (e.key === "w")
-        moveUpPlayer2();
-    else if (e.key === "s")
-        moveDownPlayer2();
-}
+        function keyDown(e) {
+            console.log(e.key);
+            if (e.key === "ArrowUp")
+                moveUpPlayer1();
+            else if (e.key === "ArrowDown")
+                moveDownPlayer1();
+            else if (e.key === "w")
+                moveUpPlayer2();
+            else if (e.key === "s")
+                moveDownPlayer2();
+        }
 
-function keyUp(e) {
-    if (
-        e.key == "ArrowUp" ||
-        e.key == "ArrowDown"
-    ) {
-        player1.dy = 0;
-    } else if (
-        e.key == "w" ||
-        e.key == "s"
-    ) {
-        player2.dy = 0;
-    }
-}
+        function keyUp(e) {
+            if (
+                e.key == "ArrowUp" ||
+                e.key == "ArrowDown"
+            ) {
+                player1.dy = 0;
+            } else if (
+                e.key == "w" ||
+                e.key == "s"
+            ) {
+                player2.dy = 0;
+            }
+        }
 
-keyDown();
-
+        keyDown();
